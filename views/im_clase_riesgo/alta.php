818d1b079d5204b3f3f1a01f84d377aa418cd197
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-11 23:32:26 -0500

    v0.13.0 Se integra modelo clase_riesgo

--- views/im_clase_riesgo/alta.php
+++ views/im_clase_riesgo/alta.php
@@ -6,7 +6,7 @@
         <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
 
         <div class="  form-main" id="form">
-            <form method="post" action="./index.php?seccion=em_empleado&accion=alta_bd&session_id=<?php echo $controlador->session_id; ?>" class="form-additional">
+            <form method="post" action="./index.php?seccion=im_clase_riesgo&accion=alta_bd&session_id=<?php echo $controlador->session_id; ?>" class="form-additional">
 
                 <?php echo $controlador->inputs->codigo; ?>
                 <?php echo $controlador->inputs->codigo_bis; ?>
