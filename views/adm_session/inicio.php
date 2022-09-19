f5dabbcfae9e5d4779ad79851f34af98158c5ec1
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-19 13:04:16 -0500

    v0.17.0 Se integra elemento tipo movimiento a vista inicio

--- views/adm_session/inicio.php
+++ views/adm_session/inicio.php
@@ -51,6 +51,17 @@ $url_assets = (new views())->url_assets;
                     </div>
                 </a>
             </div>
+            <div class="col-sm-2">
+                <a href="<?php echo $controlador->link_lista_im_tipo_movimiento; ?>">
+                    <div class="cont_imagen_accion">
+                        <img src="<?php echo $url_assets; ?>img/inicio/imagen_2.jpg">
+                    </div>
+                    <div class="cont_text_accion">
+                        <h4 class="text_seccion">Tipos de Movimiento</h4>
+                        <h4 class="text_accion">Catologo</h4>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 </div>
---
+++ views/im_tipo_movimiento/alta.php
@@ -0,0 +1,28 @@
+<?php /** @var \tglobally\tg_empleado\controllers\controlador_em_empleado $controlador */ ?>
+<?php include 'templates/im_tipo_movimiento/alta/secciones.php'; ?>
+<div class="col-md-9 formulario">
+    <div class="col-lg-12">
+
+        <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
+
+        <div class="  form-main" id="form">
+            <form method="post" action="./index.php?seccion=im_tipo_movimiento&accion=alta_bd&session_id=<?php echo $controlador->session_id; ?>" class="form-additional">
+
+                <?php echo $controlador->inputs->codigo; ?>
+                <?php echo $controlador->inputs->codigo_bis; ?>
+                <?php echo $controlador->inputs->descripcion; ?>
+                <?php echo $controlador->inputs->descripcion_select; ?>
+                <?php echo $controlador->inputs->alias; ?>
+
+                <div class="buttons col-md-12">
+                    <div class="col-md-6">
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " name="btn_action_next" value="modifica">Guarda</button>
+                    </div>
+                    <div class="col-md-6 ">
+                        <a href="index.php?seccion=im_tipo_movimiento&accion=lista&session_id=<?php echo $controlador->session_id; ?>"  class="btn btn-info btn-guarda col-md-12 ">Regresar</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
