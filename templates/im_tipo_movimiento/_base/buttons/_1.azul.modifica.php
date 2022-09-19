a025dc74fbb76ba1e7161a816bed5f90bff2af69
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-19 13:41:25 -0500

    v0.19.0 Se integra secciones de tipo_movimiento

---
+++ templates/im_tipo_movimiento/_base/buttons/_1.azul.modifica.php
@@ -0,0 +1,2 @@
+<?php include "templates/im_tipo_movimiento/_base/numero/1.azul.php"; ?>
+<?php include "templates/im_tipo_movimiento/_base/texto_menu_lateral/modifica.php"; ?>
---
+++ templates/im_tipo_movimiento/_base/links/alta.php
@@ -0,0 +1,4 @@
+<?php /** @var \tglobally\tg_empleado\controllers\controlador_em_empleado $controlador */ ?>
+<a href="index.php?seccion=im_tipo_movimiento&accion=alta&session_id=<?php echo $controlador->session_id; ?>">
+    <?php include "templates/im_tipo_movimiento/_base/buttons/1.azul.alta.php"; ?>
+</a>
