5d3fc898ba1616089d49ef8133d775be8125c9b5
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-07 11:40:58 -0500

    v0.30.1 FIX modifica_bd im registro patronal

--- views/im_registro_patronal/modifica.php
+++ views/im_registro_patronal/modifica.php
@@ -6,7 +6,7 @@
         <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
 
         <div class="  form-main" id="form">
-            <form method="post" action="./index.php?seccion=im_registro_patronal&accion=modifica_bd&session_id=<?php echo $controlador->session_id; ?>&resgistro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
+            <form method="post" action="./index.php?seccion=im_registro_patronal&accion=modifica_bd&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
 
                 <?php echo $controlador->inputs->id; ?>
                 <?php echo $controlador->inputs->descripcion; ?>
@@ -15,7 +15,7 @@
 
                 <div class="buttons col-md-12">
                     <div class="col-md-6">
-                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " name="btn_action_next" value="modifica">Guarda</button>
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " value="modifica">Guarda</button>
                     </div>
                     <div class="col-md-6 ">
                         <a href="index.php?seccion=im_registro_patronal&accion=lista&session_id=<?php echo $controlador->session_id; ?>"  class="btn btn-info btn-guarda col-md-12 ">Regresar</a>
