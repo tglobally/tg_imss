a3d32229da6ab14653029d0b1b2000a86216b1a6
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-07 11:10:00 -0500

    v0.28.0 Se agrega boton regresar en modifica tg imss

--- views/im_registro_patronal/modifica.php
+++ views/im_registro_patronal/modifica.php
@@ -18,7 +18,7 @@
                         <button type="submit" class="btn btn-info btn-guarda col-md-12 " name="btn_action_next" value="modifica">Guarda</button>
                     </div>
                     <div class="col-md-6 ">
-                        <a href="index.php?seccion=im_registro_patronal&accion=fiscales&session_id=<?php echo $controlador->session_id; ?>&resgistro_id=<?php echo $controlador->registro_id; ?>"  class="btn btn-info btn-guarda col-md-12 ">Siguiente</a>
+                        <a href="index.php?seccion=im_registro_patronal&accion=lista&session_id=<?php echo $controlador->session_id; ?>"  class="btn btn-info btn-guarda col-md-12 ">Regresar</a>
                     </div>
                 </div>
             </form>
