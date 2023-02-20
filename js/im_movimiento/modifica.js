875f578663e9a0ff90cf1637d3aa6f20a9460ef6
[user] <angel.martinez@ivitec.com.mx>
2023-02-20 14:19:22 -0600

    v0.48.6 Se integra validacion tipo movimiento

--- js/im_movimiento/modifica.js
+++ js/im_movimiento/modifica.js
@@ -3,10 +3,24 @@ let url = getAbsolutePath();
 let session_id = getParameterByName('session_id');
 
 let sl_em_empleado = $("#em_empleado_id");
+let sl_im_tipo_movimiento = $('#im_tipo_movimiento_id');
 let txt_salario_diario = $('#salario_diario');
 let txt_factor_integracion = $('#factor_integracion');
 let txt_salario_diario_integrado = $('#salario_diario_integrado');
 let txt_fecha_inicio_rel_laboral = $('#fecha');
+
+sl_im_tipo_movimiento.change(function (event) {
+    let tipo_mov = $("#im_tipo_movimiento_id option:selected").text();
+
+    if (tipo_mov === 'BAJA' || tipo_mov === 'REINGRESO') {
+        txt_salario_diario.prop('disabled', true);
+        txt_salario_diario_integrado.prop('disabled', true);
+    }
+    else{
+        txt_salario_diario.prop('disabled', false);
+        txt_salario_diario_integrado.prop('disabled', false);
+    }
+});
 
 txt_salario_diario.change(function (){
     let salario_diario = $(this).val();
@@ -16,13 +30,6 @@ txt_salario_diario.change(function (){
     txt_salario_diario_integrado.val(res.toFixed(2));
 });
 
-txt_factor_integracion.change(function (){
-    let factor = $(this).val();
-    let salario_diario = txt_salario_diario.val();
-
-    let res = salario_diario * factor;
-    txt_salario_diario_integrado.val(res.toFixed(2));
-});
 let getData = async (url, acciones) => {
     fetch(url)
         .then(response => response.json())
