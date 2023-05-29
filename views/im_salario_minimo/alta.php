6ffc20e0551514a986dbf83fc61b6602af5d1c47
Kevin <kevin.acuna@ivitec.com.mx>
2023-05-29 12:49:48 -0500

    v0.84.7 Se ajusta seccion

--- views/im_salario_minimo/alta.php
+++ views/im_salario_minimo/alta.php
@@ -1,30 +1,54 @@
 <?php /** @var \tglobally\tg_imss\controllers\controlador_im_salario_minimo $controlador */ ?>
-<?php include $controlador->include_menu_secciones; ?>
-<div class="col-md-9 formulario">
-    <div class="col-lg-12">
 
-        <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
+<div class="container-lg">
 
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item"><a href="./index.php?seccion=adm_session&accion=inicio&session_id=<?php echo $controlador->session_id ?>">Inicio</a></li>
+            <li class="breadcrumb-item"><a href="<?php echo $controlador->link_lista?>">Lista</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Alta</li>
+        </ol>
+    </nav>
 
-        <div class="  form-main" id="form">
-            <form method="post" action="./index.php?seccion=im_salario_minimo&accion=alta_bd&session_id=<?php echo $controlador->session_id; ?>" class="form-additional">
-                <?php echo $controlador->inputs->codigo; ?>
-                <?php echo $controlador->inputs->codigo_bis; ?>
-                <?php echo $controlador->inputs->descripcion; ?>
-                <?php echo $controlador->inputs->select->im_tipo_salario_minimo_id; ?>
-                <?php echo $controlador->inputs->select->dp_cp_id; ?>
-                <?php echo $controlador->inputs->fecha_inicio; ?>
-                <?php echo $controlador->inputs->fecha_fin; ?>
-                <?php echo $controlador->inputs->monto; ?>
-                <div class="buttons col-md-12">
-                    <div class="col-md-6">
-                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " >Guarda</button>
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="card mb-4">
+                <div class="card-body p-4">
+
+                    <div class="row">
+                        <div class="col">
+                            <div class="card-title fs-2 fw-semibold">Alta Salario Minimo</div>
+                        </div>
+                        <div class="col-auto ms-auto">
+                            <button class="btn btn-secondary btn-sm" type="button" data-coreui-toggle="dropdown" aria-expanded="false">
+                                <i class="bi bi-list-task icon me-2"></i>
+                                Acciones
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" href="<?php echo $controlador->link_alta?>">Alta</a></li>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="col-md-6">
-                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " >Siguiente</button>
-                    </div>
+
+                    <form class="row g-3" method="post" action="<?php echo $controlador->link_alta_bd; ?>">
+
+                        <?php echo $controlador->inputs->codigo; ?>
+                        <?php echo $controlador->inputs->codigo_bis; ?>
+                        <?php echo $controlador->inputs->descripcion; ?>
+                        <?php echo $controlador->inputs->select->im_tipo_salario_minimo_id; ?>
+                        <?php echo $controlador->inputs->select->dp_cp_id; ?>
+                        <?php echo $controlador->inputs->fecha_inicio; ?>
+                        <?php echo $controlador->inputs->fecha_fin; ?>
+                        <?php echo $controlador->inputs->monto; ?>
+
+                        <div class="col-12 d-flex justify-content-end">
+                            <button type="submit" class="btn btn-primary" name="btn_action_next">Registrar</button>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
+
 </div>
+
