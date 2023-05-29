99977e4ebffc5b3b03349e1379e3f04ef3c11af0
Kevin <kevin.acuna@ivitec.com.mx>
2023-05-29 12:06:51 -0500

    v0.83.7 Se ajusta vista para registros

--- views/im_registro_patronal/alta.php
+++ views/im_registro_patronal/alta.php
@@ -1,26 +1,49 @@
 <?php /** @var \tglobally\tg_imss\controllers\controlador_im_registro_patronal $controlador */ ?>
-<?php include 'templates/im_registro_patronal/alta/secciones.php'; ?>
-<div class="col-md-9 formulario">
-    <div class="col-lg-12">
 
-        <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
+<div class="container-lg">
 
-        <div class="  form-main" id="form">
-            <form method="post" action="./index.php?seccion=im_registro_patronal&accion=alta_bd&session_id=<?php echo $controlador->session_id; ?>" class="form-additional">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item"><a href="./index.php?seccion=adm_session&accion=inicio&session_id=<?php echo $controlador->session_id ?>">Inicio</a></li>
+            <li class="breadcrumb-item"><a href="<?php echo $controlador->link_lista?>">Lista</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Alta</li>
+        </ol>
+    </nav>
 
-                <?php echo $controlador->inputs->fc_csd_id; ?>
-                <?php echo $controlador->inputs->im_clase_riesgo_id; ?>
-                <?php echo $controlador->inputs->descripcion; ?>
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="card mb-4">
+                <div class="card-body p-4">
 
-                <div class="buttons col-md-12">
-                    <div class="col-md-6">
-                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " name="btn_action_next" value="modifica">Guarda</button>
+                    <div class="row">
+                        <div class="col">
+                            <div class="card-title fs-2 fw-semibold">Alta Registro Patronal</div>
+                        </div>
+                        <div class="col-auto ms-auto">
+                            <button class="btn btn-secondary btn-sm" type="button" data-coreui-toggle="dropdown" aria-expanded="false">
+                                <i class="bi bi-list-task icon me-2"></i>
+                                Acciones
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" href="<?php echo $controlador->link_alta?>">Alta</a></li>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="col-md-6 ">
-                        <a href="index.php?seccion=im_registro_patronal&accion=lista&session_id=<?php echo $controlador->session_id; ?>"  class="btn btn-info btn-guarda col-md-12 ">Regresar</a>
-                    </div>
+
+                    <form class="row g-3" method="post" action="<?php echo $controlador->link_alta_bd; ?>">
+
+                        <?php echo $controlador->inputs->fc_csd_id; ?>
+                        <?php echo $controlador->inputs->im_clase_riesgo_id; ?>
+                        <?php echo $controlador->inputs->descripcion; ?>
+
+                        <div class="col-12 d-flex justify-content-end">
+                            <button type="submit" class="btn btn-primary" name="btn_action_next">Registrar</button>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
+
 </div>
+
--- views/im_registro_patronal/lista.php
+++ views/im_registro_patronal/lista.php
@@ -1,27 +1,43 @@
 <?php /** @var \tglobally\tg_imss\controllers\controlador_im_registro_patronal $controlador */ ?>
-<?php
 
-use config\views;
-$url_icons = (new views())->url_icons;
-?>
+<div class="container-lg">
 
-<?php include 'templates/im_registro_patronal/lista/secciones.php'; ?>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item">
+                <a href="./index.php?seccion=adm_session&accion=inicio&session_id=<?php echo $controlador->session_id ?>">Inicio</a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">Lista</li>
+        </ol>
+    </nav>
 
-<div class="col-md-9 info-lista">
-    <div class="col-lg-12 content">
-        <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?></h3>
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="card mb-4">
+                <div class="card-body p-4">
 
-        <div class="lista">
-            <div class="card">
+                    <div class="row">
+                        <div class="col">
+                            <div class="card-title fs-2 fw-semibold">Listado de Registros Patronales</div>
+                        </div>
+                        <div class="col-auto ms-auto">
+                            <button class="btn btn-secondary btn-sm" type="button" data-coreui-toggle="dropdown" aria-expanded="false">
+                                <i class="bi bi-list-task icon me-2"></i>
+                                Acciones
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" href="<?php echo $controlador->link_alta?>">Alta</a></li>
+                            </ul>
+                        </div>
+                    </div>
 
-                <div class="card-body">
-                    <div class="cont_tabla_sucursal  col-md-12">
-                        <table class="table table-striped datatable">
-                        </table>
+                    <div class="table-responsive">
+                        <table class="table mb-0 table-striped table-sm datatable"></table>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
+
 </div>
+
