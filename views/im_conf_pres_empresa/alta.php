0acf698e1ca175b2880c1551f2f08563dc266c20
[user] <angel.martinez@ivitec.com.mx>
2023-06-01 18:23:48 -0600

    v0.100.8 Se ajustan views conf empresa

--- views/im_conf_pres_empresa/alta.php
+++ views/im_conf_pres_empresa/alta.php
@@ -3,10 +3,9 @@
 <form class="row g-3" method="post" action="<?php echo $controlador->link_alta_bd; ?>">
 
     <?php echo $controlador->inputs->codigo; ?>
-    <?php echo $controlador->inputs->codigo_bis; ?>
     <?php echo $controlador->inputs->descripcion; ?>
-    <?php echo $controlador->inputs->descripcion_select; ?>
-    <?php echo $controlador->inputs->alias; ?>
+    <?php echo $controlador->inputs->org_empresa_id; ?>
+    <?php echo $controlador->inputs->im_conf_prestaciones_id; ?>
 
     <div class="col-12 d-flex justify-content-end">
         <button type="submit" class="btn btn-primary" name="btn_action_next">Registrar</button>
