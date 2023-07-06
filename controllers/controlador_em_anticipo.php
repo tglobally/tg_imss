cc676af761d0b6d7dcddd5f9286a2241bc6853b5
Kevin <kevin.acuna@ivitec.com.mx>
2023-07-06 15:58:12 -0500

    v0.152.12 Se integran elementos de filtrado para anticipos

--- controllers/controlador_em_anticipo.php
+++ controllers/controlador_em_anticipo.php
@@ -13,7 +13,7 @@ namespace tglobally\tg_imss\controllers;
 use gamboamartin\errores\errores;
 use PDO;
 use stdClass;
-use tglobally\template_tg\html;
+use gamboamartin\system\system;
 
 class controlador_em_anticipo extends \tglobally\tg_empleado\controllers\controlador_em_anticipo {
 
@@ -31,6 +31,27 @@ class controlador_em_anticipo extends \tglobally\tg_empleado\controllers\control
             die('Error');
         }
 
+    }
+
+    protected function campos_view(): array
+    {
+        $keys = new stdClass();
+        $keys->inputs = array('codigo', 'descripcion', 'monto', 'n_pagos', 'comentarios');
+        $keys->fechas = array('fecha_prestacion', 'fecha_inicio_descuento', 'fecha_inicio', "fecha_final");
+        $keys->selects = array();
+
+        $init_data = array();
+        $init_data['em_tipo_anticipo'] = "gamboamartin\\empleado";
+        $init_data['em_empleado'] = "gamboamartin\\empleado";
+        $init_data['em_tipo_descuento'] = "gamboamartin\\empleado";
+        $init_data['org_empresa'] = "gamboamartin\\organigrama";
+
+        $campos_view = $this->campos_view_base(init_data: $init_data, keys: $keys);
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al inicializar campo view', data: $campos_view);
+        }
+
+        return $campos_view;
     }
 
     protected function init_datatable(): stdClass
@@ -51,6 +72,53 @@ class controlador_em_anticipo extends \tglobally\tg_empleado\controllers\control
         $datatables->menu_active = true;
 
         return $datatables;
+    }
+
+    public function init_selects_inputs(): array
+    {
+        $keys_selects = parent::init_selects_inputs();
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al icializar selects', data: $keys_selects);
+        }
+
+        $keys_selects = $this->init_selects(keys_selects: $keys_selects, key: "org_empresa_id", label: "Empresa:",
+            cols: 12);
+
+        return $keys_selects;
+    }
+
+    protected function key_selects_txt(array $keys_selects): array
+    {
+        $keys_selects = parent::key_selects_txt($keys_selects);
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al icializar selects', data: $keys_selects);
+        }
+
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 6, key: 'fecha_inicio',
+            keys_selects: $keys_selects, place_holder: 'Fecha Inicio', required: false);
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 6, key: 'fecha_final',
+            keys_selects: $keys_selects, place_holder: 'Fecha Final', required: false);
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+
+        return $keys_selects;
+    }
+
+    public function lista(bool $header, bool $ws = false): array
+    {
+        $alta = parent::alta($header, $ws);
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al inicializar sidebar', data: $alta);
+            print_r($error);
+            die('Error');
+        }
+
+        return (array)$alta;
     }
 
 }
--- views/em_anticipo/lista.php
+++ views/em_anticipo/lista.php
@@ -1,3 +1,57 @@
+<?php /** @var \tglobally\tg_imss\controllers\controlador_em_anticipo $controlador  controlador en ejecucion */ ?>
+
+<div class="accordion accordion-flush" id="accordionFlushExample">
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-headingOne">
+            <button class="accordion-button collapsed" type="button" data-coreui-toggle="collapse"
+                    data-coreui-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                Filtros avanzados
+            </button>
+        </h2>
+        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
+             data-coreui-parent="#accordionFlushExample">
+            <div class="accordion-body">
+
+                <form action="<?php echo $controlador->link_nom_nomina_exportar_nominas; ?>" method="post" class="form-additional" id="form_export">
+                    <div class="card-title fs-6 fw-semibold">Seleccione una opción de busqueda:</div>
+                    <div>
+                        <div class="card-subtitle text-disabled">Seleccione una categoría</div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="categorias" id="radio-empresa"
+                                   value="org_empresa" checked data-accion="get_empresa" data-titulo="Empresa:">
+                            <label class="form-check-label" for="radio-empresa">Empresa</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="categorias" id="radio-remunerado"
+                                   value="em_empleado" data-accion="get_empleado" data-titulo="Remunerado:">
+                            <label class="form-check-label" for="radio-remunerado">Remunerado</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="categorias" id="radio-usuario"
+                                   value="adm_usuario" data-accion="get_usuario" data-titulo="Ejecutivos:">
+                            <label class="form-check-label" for="radio-remunerado">Ejecutivo</label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <?php echo $controlador->inputs->org_empresa_id; ?>
+                            <div class="col-sm-12">
+                                <?php echo $controlador->inputs->fecha_inicio; ?>
+                                <?php echo $controlador->inputs->fecha_final; ?>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary export" name="btn_action_next"
+                            style="border-radius: 5px" value="exportar" form="form_export">
+                        Exportar
+                    </button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
 <div class="table-responsive">
     <table class="table mb-0 table-striped table-sm datatable"></table>
 </div>
